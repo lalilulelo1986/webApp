beaeb10e602599ece3a73ea37b1edb075b656f5e
gituser <lalilulelo_86@list.ru>
2017-06-17 00:47:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lalafa on 16.06.17.
+ */
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello API');
+})
+
+app.listen(3012, function () {
+    console.log('API app started');
+})
