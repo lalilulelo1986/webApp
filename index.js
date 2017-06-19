398cdaf38f2e0fa56d508a508305384b278be455
gituser <lalilulelo_86@list.ru>
2017-06-19 21:55:12 +0600

    db file

--- index.js
+++ index.js
@@ -3,14 +3,18 @@
  */
 var express = require('express');
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
+//var MongoClient = require('mongodb').MongoClient;
 var ObjectId = require('mongodb').ObjectId;
-
+var db = require('./db');
+var sleep = require('sleep');
 var app = express();
-var db;
+//var db;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 var artists = [
     {id: 1, name: 'Queen'},
@@ -23,84 +27,70 @@ app.get('/', function (req, res) {
 })
 
 app.get('/artists', function (req, res) {
-    //res.send(artists);
-    db.collection('artists').find().toArray(function (err, docs) {
+    sleep.sleep(5);
+    db.get().collection('artists').find().toArray(function (err, docs) {
         if (err){
             console.log(err);
             return res.sendStatus(500);
         }
+
         res.send(docs);
     })
 })
 
 app.get('/artists/:id', function (req, res) {
-    db.collection('artists').findOne({_id: ObjectId(req.params.id)}, function (err, doc) {
+    db.get().collection('artists').findOne({_id: ObjectId(req.params.id)}, function (err, doc) {
         if (err){
             console.log(err);
             return req.sendStatus(500);
         }
         res.send(doc);
     })
-    /*var artist = artists.find(function (artist) {
-        return artist.id === Number(req.params.id)
-    });*/
-    //res.send(artist);
 })
 
 app.post('/artists', function (req, res) {
     var artist = {
         name: req.body.name
     }
-    db.collection('artists').insert(artist, function (err, result) {
+    db.get().collection('artists').insert(artist, function (err, result) {
         if (err){
             console.log(err);
             return res.sendStatus(500);
         }
         res.send(artist);
     })
-    /*
-    if (artists.push(artist)) {
-        res.send(artist);
-    }*/
 })
 
 app.put('/artists/:id', function (req, res) {
-    db.collection('artists').update({_id: ObjectId(req.params.id)}, {name:req.body.name});
-    res.sendStatus(200);
-    /*
-    var artist = artists.find(function (artist) {
-        return artist.id === Number(req.params.id)
+    db.get().collection('artists').updateOne({_id: ObjectId(req.params.id)}, {name:req.body.name}, function (err, result) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.sendStatus(200);
     });
-    artist.name = req.body.name;
-    res.sendStatus(200);*/
 })
 
 app.delete('/artists/:id', function (req, res) {
-    //artists.remove();
-    var artist = artists.find(function (artist) {
-        return artist.id === Number(req.params.id)
+    db.get().collection('artists').deleteOne({_id: ObjectId(req.params.id)}, function (err, result) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.sendStatus(200);
     });
-    var index = artists.indexOf(artist);
-    artists.splice(index, 1);
-    console.log('/*-------------');
-    console.log(artist);
-    console.log('удален');
-    console.log('-------------*/');
-    res.sendStatus(200);
 })
-
-/*app.listen(3012, function () {
-    console.log('API app started');
-})*/
 
 //192.168.1.66 remote ip
 //localhost    local
-MongoClient.connect('mongodb://localhost:27017/mongo', function (err, database) {
+db.connect('mongodb://localhost:27017/mongo', function (err) {
     if (err){
         return console.log(err);
     }
-    db = database;
+    //db = database;
     app.listen(3012, function () {
         console.log('API app started');
     })
+
 })
+
---
+++ server.js
@@ -0,0 +1,4 @@
+/**
+ * Created by lalafa on 18.06.17.
+ */
+console.log('npm start- runs this file');
