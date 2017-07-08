6a8407902511c71fe5a08496ae27243f445a1d8e
gituser <lalilulelo_86@list.ru>
2017-07-09 01:09:10 +0600

    +angular and music data

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ var ObjectId = require('mongodb').ObjectId;
 var db = require('./db');
 //var sleep = require('sleep');
 var app = express();
+app.use(express.static('/app'));
 //var db;
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static('app'));
+//app.use(express.static('files'));
 
 
 var artists = [
@@ -22,8 +24,9 @@ var artists = [
     {id: 3, name: 'Rammstein'}
 ];
 
-app.get('/', function (req, res) {
-    res.send('Hello API');
+app.get('/$', function (req, res) {
+    res.sendFile(__dirname + '/app/index.html');
+    //res.send('Hello API');
 })
 
 app.get('/artists', function (req, res) {
@@ -50,7 +53,8 @@ app.get('/artists/:id', function (req, res) {
 
 app.post('/artists', function (req, res) {
     var artist = {
-        name: req.body.name
+        name: req.body.name,
+        album: req.body.album
     }
     db.get().collection('artists').insert(artist, function (err, result) {
         if (err){
@@ -73,6 +77,61 @@ app.put('/artists/:id', function (req, res) {
 
 app.delete('/artists/:id', function (req, res) {
     db.get().collection('artists').deleteOne({_id: ObjectId(req.params.id)}, function (err, result) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.sendStatus(200);
+    });
+})
+/********FOR MUSIC*****/
+app.get('/music', function (req, res) {
+    //sleep.sleep(5);
+    db.get().collection('music').find().toArray(function (err, docs) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(docs);
+    })
+})
+
+app.get('/music/:id', function (req, res) {
+    db.get().collection('music').findOne({_id: ObjectId(req.params.id)}, function (err, doc) {
+        if (err){
+            console.log(err);
+            return req.sendStatus(500);
+        }
+        res.send(doc);
+    })
+})
+
+app.post('/music', function (req, res) {
+    var music = [{
+        name: req.body.name,
+        album: req.body.album
+    }];
+    db.get().collection('music').insert(music, function (err, result) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(music);
+    })
+})
+
+app.put('/music/:id', function (req, res) {
+    db.get().collection('music').updateOne({_id: ObjectId(req.params.id)}, {name:req.body.name}, function (err, result) {
+        if (err){
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.sendStatus(200);
+    });
+})
+
+app.delete('/music/:id', function (req, res) {
+    db.get().collection('music').deleteOne({_id: ObjectId(req.params.id)}, function (err, result) {
         if (err){
             console.log(err);
             return res.sendStatus(500);
