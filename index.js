b4fed60efa283e4e0ce3c361b3de862c4f832632
lalilulelo1986 <lalilulelo_86@list.ru>
2017-08-18 22:37:14 +0600

    db for postgres
    db for postgres

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 //var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
+//var ObjectId = require('mongodb').ObjectId;
 var db = require('./db');
+var artists = require('./artists');
 //var sleep = require('sleep');
 var app = express();
 app.use(express.static('/app'));
@@ -16,12 +17,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('app'));
 
-
-var artists = [
-    {id: 1, name: 'Queen'},
-    {id: 2, name: 'Pind Floyd'},
-    {id: 3, name: 'Rammstein'}
-];
+artists.init(app);
+// var artists = [
+//     {id: 1, name: 'Queen'},
+//     {id: 2, name: 'Pind Floyd'},
+//     {id: 3, name: 'Rammstein'}
+// ];
 
 app.get('/$', function (req, res) {
     res.sendFile(__dirname + '/app/index.html');
@@ -33,62 +34,23 @@ app.get('/resolve$', function (req, res) {
     //res.send('Hello API');
 })
 
-app.get('/artists', function (req, res) {
-    //sleep.sleep(5);
-    db.get().collection('artists').find().toArray(function (err, docs) {
-        if (err){
-            console.log(err);
-            return res.sendStatus(500);
-        }
 
-        res.send(docs);
+app.get('/artists2', function (req, res) {
+    const query = {
+      text: 'select id, name from artists'    
+    };
+    db.get().query(query, (err, result) => {
+      if (err) {
+        return res.sendStatus(500);
+      } else {
+        res.send(result.rows);
+      }
     })
 })
 
-app.get('/artists/:id', function (req, res) {
-    db.get().collection('artists').findOne({_id: ObjectId(req.params.id)}, function (err, doc) {
-        if (err){
-            console.log(err);
-            return req.sendStatus(500);
-        }
-        res.send(doc);
-    })
-})
 
-app.post('/artists', function (req, res) {
-    var artist = {
-        name: req.body.name,
-        album: req.body.album
-    }
-    db.get().collection('artists').insert(artist, function (err, result) {
-        if (err){
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        res.send(artist);
-    })
-})
-
-app.put('/artists/:id', function (req, res) {
-    db.get().collection('artists').updateOne({_id: ObjectId(req.params.id)}, {name:req.body.name}, function (err, result) {
-        if (err){
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        res.sendStatus(200);
-    });
-})
-
-app.delete('/artists/:id', function (req, res) {
-    db.get().collection('artists').deleteOne({_id: ObjectId(req.params.id)}, function (err, result) {
-        if (err){
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        res.sendStatus(200);
-    });
-})
-/********FOR MUSIC*****/
+/*
+*******FOR MUSIC****
 app.get('/music', function (req, res) {
     //sleep.sleep(5);
     db.get().collection('music').find().toArray(function (err, docs) {
@@ -143,10 +105,10 @@ app.delete('/music/:id', function (req, res) {
         res.sendStatus(200);
     });
 })
-
+*/
 //192.168.1.66 remote ip
 //localhost    local
-db.connect('mongodb://localhost:27017/mongo', function (err) {
+db.connect("postgres://postgres:postgres@localhost:5432/postgres", function (err) {
     if (err){
         return console.log(err);
     }
